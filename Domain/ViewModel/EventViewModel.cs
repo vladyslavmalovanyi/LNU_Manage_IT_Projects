5558ac0aab3f5ad71ab900e9cd0adf2abca5944a
Vira Sydoriak <vsydoriak99@gmail.com>
2020-12-21 12:34:34 +0200

    fix datame format

--- Domain/ViewModel/EventViewModel.cs
+++ Domain/ViewModel/EventViewModel.cs
@@ -6,10 +6,28 @@ namespace Domain.ViewModel
 {
     public class EventViewModel : BaseDomain
     {
-        public string Name { get; set; }
-        public DateTime DateStart { get; set; }
+        private DateTime dateStart;
+        private DateTime dateFinish;
 
-        public DateTime DateFinish { get; set; }
+        public string Name { get; set; }
+        public DateTime DateStart {
+            get {
+                return new DateTime(dateStart.Year, dateStart.Month, dateStart.Day, dateStart.Hour, dateStart.Minute, dateStart.Second);
+            }
+            set { dateStart = value;
+            } 
+        }
+
+        public DateTime DateFinish {
+            get
+            {
+                return new DateTime(dateFinish.Year, dateFinish.Month, dateFinish.Day, dateFinish.Hour, dateFinish.Minute, dateFinish.Second);
+            }
+            set
+            {
+                dateFinish = value;
+            }
+        }
 
         public string Location { get; set; }
         public string Decription { get; set; }
